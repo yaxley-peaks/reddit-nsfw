1c51bd5597d25a30cd25b1422e81245d674b019d
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-07-08 23:40:04 +0530

    works for all orderings now

--- src/lib.rs
+++ src/lib.rs
@@ -33,9 +33,87 @@ pub fn get_posts(sub: &str, _sort: SortBy, lim: u32) -> Vec<String> {
                 after = dat.data.after.unwrap();
             }
         }
-        SortBy::Hot => todo!(),
-        SortBy::Rising => todo!(),
-        SortBy::New => todo!(),
+        SortBy::Hot => {
+            let dat = sub.hot(lim, None).unwrap();
+
+            dat.data.children.iter().for_each(|thing| {
+                if thing.data.over_18 {
+                    res.push(thing.data.permalink.clone());
+                }
+            });
+            //TODO: Fix this later
+            let mut after = dat.data.after.unwrap();
+            loop {
+                if res.len() >= lim as usize {
+                    break;
+                }
+                println!("got {} posts", res.len());
+                let _a_opt = FeedOption::new().after(&after);
+
+                let dat = sub.hot(lim, Some(_a_opt)).unwrap();
+
+                dat.data.children.iter().for_each(|thing| {
+                    if thing.data.over_18 {
+                        res.push(thing.data.permalink.clone());
+                    }
+                });
+                after = dat.data.after.unwrap();
+            }
+        }
+        SortBy::Rising => {
+            let dat = sub.rising(lim, None).unwrap();
+
+            dat.data.children.iter().for_each(|thing| {
+                if thing.data.over_18 {
+                    res.push(thing.data.permalink.clone());
+                }
+            });
+            //TODO: Fix this later
+            let mut after = dat.data.after.unwrap();
+            loop {
+                if res.len() >= lim as usize {
+                    break;
+                }
+                println!("got {} posts", res.len());
+                let _a_opt = FeedOption::new().after(&after);
+
+                let dat = sub.rising(lim, Some(_a_opt)).unwrap();
+
+                dat.data.children.iter().for_each(|thing| {
+                    if thing.data.over_18 {
+                        res.push(thing.data.permalink.clone());
+                    }
+                });
+                after = dat.data.after.unwrap();
+            }
+        }
+        SortBy::New => {
+            let dat = sub.latest(lim, None).unwrap();
+
+            dat.data.children.iter().for_each(|thing| {
+                if thing.data.over_18 {
+                    res.push(thing.data.permalink.clone());
+                }
+            });
+            //TODO: Fix this later
+            let mut after = dat.data.after.unwrap();
+            loop {
+                if res.len() >= lim as usize {
+                    break;
+                }
+                println!("got {} posts", res.len());
+                let _a_opt = FeedOption::new().after(&after);
+
+                let dat = sub.latest(lim, Some(_a_opt)).unwrap();
+
+                dat.data.children.iter().for_each(|thing| {
+                    if thing.data.over_18 {
+                        res.push(thing.data.permalink.clone());
+                    }
+                });
+                after = dat.data.after.unwrap();
+            }
+        }
     }
     res
 }
