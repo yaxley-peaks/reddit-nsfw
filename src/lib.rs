1c6dd4e54dcae69d60bf4c5f58d53dfbb19eb75d
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-07-08 23:33:48 +0530

    get actually that many posts now

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use crate::args::SortBy;
-use roux::Subreddit;
+use roux::{util::FeedOption, Subreddit};
 pub mod args;
 
 pub fn get_posts(sub: &str, _sort: SortBy, lim: u32) -> Vec<String> {
@@ -8,37 +8,34 @@ pub fn get_posts(sub: &str, _sort: SortBy, lim: u32) -> Vec<String> {
     match _sort {
         SortBy::Top => {
             let dat = sub.top(lim, None).unwrap();
+
             dat.data.children.iter().for_each(|thing| {
                 if thing.data.over_18 {
                     res.push(thing.data.permalink.clone());
                 }
             });
-        }
-        SortBy::Hot => {
-            let dat = sub.hot(lim, None).unwrap();
-    dat.data.children.iter().for_each(|thing| {
-        if thing.data.over_18 {
-            res.push(thing.data.permalink.clone());
-        }
-    });
-        },
-        SortBy::Rising => {
-            let dat = sub.rising(lim, None).unwrap();
-    dat.data.children.iter().for_each(|thing| {
-        if thing.data.over_18 {
-            res.push(thing.data.permalink.clone());
-        }
-    });
-        },
-        SortBy::New => {
-            let dat = sub.latest(lim, None).unwrap();
-    dat.data.children.iter().for_each(|thing| {
-        if thing.data.over_18 {
-            res.push(thing.data.permalink.clone());
-        }
-    });
-        },
-    }
+            //TODO: Fix this later
+            let mut after = dat.data.after.unwrap();
+            loop {
+                if res.len() >= lim as usize {
+                    break;
+                }
+                println!("got {} posts", res.len());
+                let _a_opt = FeedOption::new().after(&after);
 
+                let dat = sub.top(lim, Some(_a_opt)).unwrap();
+
+                dat.data.children.iter().for_each(|thing| {
+                    if thing.data.over_18 {
+                        res.push(thing.data.permalink.clone());
+                    }
+                });
+                after = dat.data.after.unwrap();
+            }
+        }
+        SortBy::Hot => todo!(),
+        SortBy::Rising => todo!(),
+        SortBy::New => todo!(),
+    }
     res
 }
